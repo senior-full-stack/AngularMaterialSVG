9deb26e9ccdb191067f2ea73b6212d2cdd8f2d23
senior-full-stack <web.expert007@hotmail.com>
2019-08-27 03:52:34 +0300

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+    title = 'angicon';
+
+    constructor(
+            private matIconRegistry: MatIconRegistry,
+            private domSanitizer: DomSanitizer) {
+        this.matIconRegistry.addSvgIcon(
+            'airplane',
+            this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/airplane.svg')
+        );
+    }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
